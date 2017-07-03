4ec2e03884eba4de174b2a43828bf1f35967bbb6
ykushch <yuri.kushch@gmail.com>
2017-07-03 10:45:58 +0300

    Add package-info file

---
+++ src/main/java/com/ykushch/solution/ch01/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Is Unique: Implement an algorithm to determine if a string has all unique characters.
+ * What if you cannot use additional data structures.
+ */
+package com.ykushch.solution.ch01;
